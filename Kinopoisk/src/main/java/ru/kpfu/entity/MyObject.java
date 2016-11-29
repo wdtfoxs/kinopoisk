bc0b5707f817653a56083edf7b79555a39257b04
VladMatyunin <vlad.matyunin96@gmail.com>
2016-11-29 23:00:56 +0300

    elastic+security

--- Kinopoisk/src/main/java/ru/kpfu/entity/MyObject.java
+++
@@ -1,8 +0,0 @@
-package ru.kpfu.entity;
-
-/**
- * Created by Vlad.M on 23.11.2016.
- */
-public interface MyObject {
-    Long getId();
-}
--- Kinopoisk/src/main/java/ru/kpfu/entity/Role.java
+++
@@ -1,8 +0,0 @@
-package ru.kpfu.entity;
-
-/**
- * Created by Vlad.M on 23.11.2016.
- */
-public enum Role {
-    ACTOR,DIRECTOR, SCENARIST,PRODUCER
-}
